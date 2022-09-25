5001b0fb83c731d3f2df1a2cceb9dd92006122e2
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2022-09-25 13:21:20 -0500

    add data model

---
+++ TulWebAPI/TulWebAPI/Models/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace TulWebAPI.Models
+{
+    public class Data
+    {
+        public long Id { get; set; }
+        public string Front { get; set; }
+        public string Back { get; set; }
+    }
+}
